1922dfb08974b4ddfb922b3ffedc80e4ec9ecee7
史艳龙 <shiyanlong_2005@126.com>
2017-05-01 16:23:26 +0800

    add

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("hello wrold!");
+            Console.WriteLine("hello wrold11!");
         }
     }
 }
