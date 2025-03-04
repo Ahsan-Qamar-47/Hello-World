b2b618c78c9fc16d9f75092b44664e53aaa9b7a3
Ahsan-Qamar-47 <ahsanqamar926@gmail.com>
2025-03-04 18:52:42 +0500

    Create info.cs

---
+++ info.cs
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Name: Nimra" << endl;
+    cout << "Roll Number: XYZ123" << endl;
+    cout << "Department: Software Engineering" << endl;
+    return 0;
+}
